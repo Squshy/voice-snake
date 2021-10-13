c1ddaacda89d4f10ce503008c005d509ce56cd1b
Unknown <Calvin.lapp@dcmail.ca>
2021-10-13 14:21:27 -0400

    woops there was actual proper spelling there

--- components/GitHubLink.tsx
+++ components/GitHubLink.tsx
@@ -6,7 +6,7 @@ interface GitHubLinkProps {}
 export const GitHubLink: React.FC<GitHubLinkProps> = ({}) => {
   return (
     <a
-      href="https://github.com/Squshy/voice-snek"
+      href="https://github.com/Squshy/voice-snake"
       aria-label="Project's GitHub repository"
     >
       <GitHubIcon className="w-4 h-4 md:h-6 md:w-6 text-gray-400 hover:scale-125 transform duration-200 ease-out hover:text-white" />
